4e2f82af12a84ad2b9ebe8130f680455bf7662bd
caselongo <caselongo@gmail.com>
2022-11-01 19:15:36 +0100

    added UpdateRecords

--- Record.go
+++ Record.go
@@ -18,8 +18,43 @@ type Records struct {
 
 type Record struct {
 	Id          string                     `json:"id"`
-	Fields      map[string]json.RawMessage `json:"fields"`
-	CreatedTime a_types.DateTimeString     `json:"createdTime"`
+	Fields      map[string]json.RawMessage `json:"-"`
+	FieldsRaw   json.RawMessage            `json:"fields"`
+	CreatedTime *a_types.DateTimeString    `json:"createdTime,omitempty"`
+}
+
+func (r *Record) UnmarshalJSON(data []byte) error {
+	type R Record
+	if err := json.Unmarshal(data, (*R)(r)); err != nil {
+		return err
+	}
+
+	if err := json.Unmarshal(r.FieldsRaw, &r.Fields); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type UpdateRecordsConfig struct {
+	BaseId    string
+	TableName string
+	Records   []Record
+}
+
+// UpdateRecords updates up to 10 records
+//
+func (service *Service) UpdateRecords(config *UpdateRecordsConfig) *errortools.Error {
+	requestConfig := go_http.RequestConfig{
+		Method: http.MethodPatch,
+		Url:    service.url(config.BaseId, config.TableName, ""),
+		BodyModel: struct {
+			Records []Record `json:"records"`
+		}{config.Records},
+	}
+	_, _, e := service.httpRequest(&requestConfig)
+
+	return e
 }
 
 type GetRecordsConfig struct {
